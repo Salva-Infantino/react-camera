a6282579746edb750272bdfeb35c364ed976b644
ingrwf <ingrwf@cpg-453071.cepegra2008.lan>
2019-09-25 10:50:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  onTakePhoto(dataUri) {
+    // Do stuff with the dataUri photo...
+    console.log(dataUri);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Camera
+          onTakePhoto={dataUri => {
+            this.onTakePhoto(dataUri);
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
