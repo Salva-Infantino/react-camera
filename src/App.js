b62b21dbc681b662254cd1aa1d716b5b13cee0ee
ingrwf <ingrwf@cpg-453071.cepegra2008.lan>
2019-09-25 11:01:00 +0200

    test function

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React, { Component } from "react";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 
-class App extends Component {
-  onTakePhoto(dataUri) {
+export default function App() {
+  const onTakePhoto = dataUri => {
     // Do stuff with the dataUri photo...
     console.log(dataUri);
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <Camera
-          onTakePhoto={dataUri => {
-            this.onTakePhoto(dataUri);
-          }}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Camera
+        onTakePhoto={dataUri => {
+          onTakePhoto(dataUri);
+        }}
+      />
+    </div>
+  );
 }
-
-export default App;
